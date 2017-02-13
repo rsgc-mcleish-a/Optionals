efcd639709313fe2f3e5bff74e895bd6a56f2160
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-13 16:50:50 -0500

    Revised for clarity and Swift 3 compatibility

---
+++ Optionals/Exploring Optionals.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+/*:
+ 
+ ## Variable initialization
+ 
+ What is the difference between an optional String and a regular String?
+ 
+ Try typing the following code into the playground:
+ 
+ ![declare-no-value](declare-no-value.png)
+ 
+ Run the playground.
+ 
+ What is the result?
+ 
+ **NOTE:** You will need to show the Debug area by pressing Command-Shift-Y.
+ 
+ */
+// Type the code below this line
+
+
+
+/*:
+ 
+ In the debug area (be sure to scroll up to the top of the output) you should have seen output similar to the following:
+ 
+ ![variable-not-initialized](error.png)
+ 
+ Now try typing the following:
+ 
+ ![declare-with-value](declare-with-value.png)
+ 
+ Run the playground again.
+ 
+ How does the result differ?
+ 
+ */
+// Type the next section of code below this line
+
+
+
+//: [Next](@next)
