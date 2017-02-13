bb605b72b39b3e8da27910220c6d26b139520645
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-13 13:44:06 -0500

    Initial Commit

---
+++ Optionals/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Optionals
+//
+//  Created by Russell Gordon on 2/13/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
